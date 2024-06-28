97e590cfa952d2403f2997351c37f22569301d43
Johann Roth <johann.roth.e46@googlemail.com>
2024-06-28 11:30:00 +0200

    added audio

--- script.js
+++ script.js
@@ -107,6 +107,8 @@ let questions = [
 
 let currentQuestion = 0;
 let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -145,10 +147,12 @@ function answer(selection) {
 
     if (selectedQuestionNumber == question['right_answer']) { // Richtige Frage beantwortet
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        AUDIO_SUCCESS.play();
         rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
     document.getElementById('next-button').disabled = false;
 }
@@ -171,4 +175,13 @@ function resetAnswerButtons() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+}
+
+function restartGame(){
+    document.getElementById('header-image').src = './img/brainbg.jpg';
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
+    document.getElementById('question-body').style = ''; //questionBody wieder anzeigen
+    document.getElementById('end-screen').style = 'display: none'; //Endscreen ausblenden
 }
